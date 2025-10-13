2facb5315889bb21c40929c2174e04fde9ab1161
akshaybabu46 <108018666+akshaybabu46@users.noreply.github.com>
2025-10-13 23:42:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, (0.0.0.0) => {
+app.listen(PORT, '0.0.0.0' , () => {
   console.log(`🚀 Web Game running at http://0.0.0.0:${PORT}`);
 });
